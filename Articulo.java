78ef1a2907015517637e76bc58d43103447f70a7
Jano-Ch <janochiambretto@gmail.com>
2026-06-15 00:11:04 -0300

    articulo to csv y constructor de articulo 2

--- Articulo.java
+++ Articulo.java
@@ -22,10 +22,25 @@ public class Articulo {
         this.categoria = categoria; 
     }
 
-    // otro constructor para cuando se lee
+    Articulo(Integer idArticulo, Integer dniAutor, String titulo, String detalle, String fecha, Categoria categoria){
+        if (idArticulo >= idCounter) {
+            idCounter = idArticulo + 1;
+        }
+        this.idArticulo = idArticulo;
+        this.dniAutor = dniAutor;
+        this.titulo = titulo;
+        this.detalle = detalle;
+        String[] fechaPartes = fecha.split("-");
+        this.fecha = LocalDate.of(Integer.parseInt(fechaPartes[0]), Integer.parseInt(fechaPartes[1]), Integer.parseInt(fechaPartes[2]));
+        comentarios = new ArrayList<Comentario>();
+        this.categoria = categoria; 
+    }
 
     public void agregarComentario(Comentario comentario){
         comentarios.add(comentario);
     }
     
+    public String toCsv() {
+        return idArticulo + "," + dniAutor + "," + titulo + "," + detalle + "," + fecha + "," + categoria;
+    }
 }
