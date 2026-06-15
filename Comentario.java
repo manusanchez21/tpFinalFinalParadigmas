d65b8a2ee6178fc542e9a3b2728761358d08ccef
Manu Sanchez Fossa <manuelsanchezfossa@gmail.com>
2026-06-15 11:31:05 -0300

    Comentario con idArticulo

--- Comentario.java
+++ Comentario.java
@@ -1,13 +1,15 @@
 public class Comentario {
     private Integer dniUsuario;
+    private Integer idArticulo;
     private String texto;
     
-    Comentario(Integer dniUsuario, String texto){
+    Comentario(Integer dniUsuario, Integer idArticulo, String texto){
         this.dniUsuario = dniUsuario;
+        this.idArticulo = idArticulo;
         this.texto = texto;
     }
 
     public String toCsv() {
-        return dniUsuario + "," + texto;
+        return dniUsuario + "," + idArticulo + "," + texto;
     }
 }
